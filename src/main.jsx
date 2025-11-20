87eeb43c40e2406e9c027e99d36e643b8a5ae400
CoffeePuma55644 <coffeepuma55644@gmail.com>
2025-11-20 15:44:30 +0100

    Ajout des dépendances '@vercel/analytics' et '@vercel/speed-insights' dans package.json, mise à jour de pnpm-lock.yaml, et intégration des composants d'analyse dans main.jsx.

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 import '@fontsource/inter/300.css'
 import '@fontsource/inter/400.css'
@@ -15,5 +17,7 @@ import '@fontsource/playfair-display/400-italic.css'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Analytics />
+    <SpeedInsights />
   </StrictMode>,
 )
